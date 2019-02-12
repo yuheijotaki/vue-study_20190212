b449da121ccc619e5825a3f9c602eddce824a229
Yuhei Jotaki <yuheijotaki@gmail.com>
2019-02-12 10:09:35 +0900

    update: カテゴリーの記事一覧表示まで

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,20 +1,22 @@
 <template>
   <div id="app">
-    <ul>
-      <li v-for="post in posts" :key="post.title.rendered">
-        <a :href="post.link" v-html="post.title.rendered"></a>
-        <!-- <pre v-html="post.content.rendered"></pre> -->
-      </li>
-    </ul>
-    <button
-      class="button"
-      :class="[{
-        'is-loading': loading,
-        'is-disabled': disabled
-      }]"
-      :disabled="disabled"
-      @click="load"
-    >次の記事を読み込む</button>
+    <header>
+      <h1>blog.yuheijotaki.com</h1>
+      <nav>
+        <ul class="category_list">
+          <li><a href="#" data-category-id="2" v-on:click="filterCategory">Develop</a></li>
+          <li><a href="#" data-category-id="3" v-on:click="filterCategory">Design</a></li>
+          <li><a href="#" data-category-id="4" v-on:click="filterCategory">Others</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <ul class="post_list">
+        <li v-for="post in posts" :key="post.title.rendered">
+          <a :href="post.link" v-html="post.title.rendered"></a>
+        </li>
+      </ul>
+    </main>
   </div>
 </template>
 
@@ -23,15 +25,15 @@
 import "normalize.css";
 // Ajax通信ライブラリ
 import axios from "axios";
+// AjaxのURL定義
+const url = `https://blog.yuheijotaki.com/wp-json/wp/v2/posts?per_page=20&categories=2`;
 
 export default {
   name: "App",
   data() {
     return {
       posts: [],
-      page: 0,
-      loading: false,
-      disabled: false,
+      page: 0
     }
   },
   mounted() {
@@ -39,27 +41,32 @@ export default {
   },
   watch: {
     page() {
-      const url = `https://blog.yuheijotaki.com/wp-json/wp/v2/posts?categories=2&per_page=20&page=${this.page}`;
       (async () => {
         try {
           const res = await axios.get(url);
           this.posts = this.posts.concat(res.data);
-          this.loading = false;
         } catch (error) {
-          console.log(error);
-          this.empty();
+          alert('取得できませんでした。')
         }
       })();
-    }
+    },
   },
   methods: {
-    load() {
-      this.loading = true;
-      this.page++;
-    },
-    empty() {
-      this.loading = false;
-      this.disabled = true;
+    filterCategory: function(event) {
+      this.page = 1;
+      this.posts.splice(0, 20); // 投稿をすべて削除
+      const cateogoryId = event.currentTarget.getAttribute('data-category-id'); // カテゴリーの取得
+      const updatedUrl = url + '&categories=' + cateogoryId; // JSON URLのアップデート
+      // console.log(updatedUrl);
+      (async () => {
+        try {
+          const res = await axios.get(updatedUrl);
+          this.posts = this.posts.concat(res.data);
+          event.target.className += ' is-current';
+        } catch (error) {
+          alert('取得できませんでした。')
+        }
+      })();
     }
   },
 };
@@ -73,33 +80,42 @@ html,* {
 #app {
   max-width: 800px;
   margin: 40px auto;
-  ul {
-    padding-left: 1.4em;
-    li {
-      margin-bottom: 5px;
-      font-size: 14px;
+  header {
+    h1 {
+      font-size: 20px;
       line-height: 1.2;
-      &:last-child {
-        margin-bottom: 0;
-      }
-      a {
-      }
-      pre {
-        font-size: 10px;
+    }
+    .category_list {
+      margin-top: 20px;
+      list-style: none;
+      display: flex;
+      li {
+        font-size: 14px;
+        line-height: 1.2;
+        margin-right: 10px;
+        &:last-child {
+          margin-right: 0;
+        }
       }
     }
   }
-  button {
-    padding: 6px 12px;
+  main {
     margin-top: 20px;
-    font-size: 13px;
-    line-height: 1.2;
-    &.is-loading {
-      background: red;
-    }
-    &.is-disabled {
-      color: #aaa;
-      background: #ccc;
+    .post_list {
+      padding-left: 1.4em;
+      li {
+        margin-bottom: 5px;
+        font-size: 14px;
+        line-height: 1.2;
+        &:last-child {
+          margin-bottom: 0;
+        }
+        a {
+        }
+        pre {
+          font-size: 10px;
+        }
+      }
     }
   }
 }
